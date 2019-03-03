e7c7356798dfd1cb24abadbbd8c18b20d41ae588
Ghevond <ghevond.98@gmail.com>
2019-03-03 14:21:57 +0400

    histogram equalization

--- HistogramEqualization/Main_Equalization.cpp
+++ HistogramEqualization/Main_Equalization.cpp
@@ -1,20 +1,65 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <string>
+#include <vector>
 
-namespace
-{
-	std::string input_path = "Files/HistogramEqualization/Example.png";
-	std::string output_path = "Files/HistogramEqualization/Result.png";
 
-	void equalize(cv::Mat& img) {
-		//todo
+const std::string input_path = "Files/HistogramEqualization/Example.png";
+const std::string output_path = "Files/HistogramEqualization/Result.png";
+
+const int GrayLevel = 256;
+
+void equalize(cv::Mat& img) {
+
+	double hist[GrayLevel];
+
+	for (size_t i = 0; i < GrayLevel; i++)
+	{
+		hist[i] = 0;
+	}
+
+	for (int i = 0; i < img.rows; i++)
+		for (int j = 0; j < img.cols; j++)
+			hist[img.at<cv::Vec3b>(i, j)[0]]++;
+
+	int total = img.rows * img.cols;
+
+	for (int i = 1; i < GrayLevel; i++)
+		hist[i] = hist[i - 1] + hist[i];
+
+	for (int i = 0; i < GrayLevel; i++) {
+		hist[i] = (hist[i] / total) * (GrayLevel - 1);
+		hist[i] = (int)(hist[i] + (0.5));
+	}
+
+	for (size_t i = 0; i < img.rows; i++)
+	{
+		for (size_t j = 0; j < img.cols; j++)
+		{
+			img.at<cv::Vec3b>(i, j)[0] = hist[img.at<cv::Vec3b>(i, j)[0]];
+			img.at<cv::Vec3b>(i, j)[1] = hist[img.at<cv::Vec3b>(i, j)[1]];
+			img.at<cv::Vec3b>(i, j)[2] = hist[img.at<cv::Vec3b>(i, j)[2]];
+		}
 	}
 
 }
 
 int main() {
 	cv::Mat img = cv::imread(input_path);
+
+	for (size_t i = 0; i < img.rows; i++)
+	{
+		for (size_t j = 0; j < img.cols; j++)
+		{
+			int avg = (img.at<cv::Vec3b>()[0] +
+				img.at<cv::Vec3b>()[1] +
+				img.at<cv::Vec3b>()[2]) / 3;
+
+			img.at<cv::Vec3b>()[0] = avg;
+			img.at<cv::Vec3b>()[1] = avg;
+			img.at<cv::Vec3b>()[2] = avg;
+		}
+	}
 
 	equalize(img);
 
