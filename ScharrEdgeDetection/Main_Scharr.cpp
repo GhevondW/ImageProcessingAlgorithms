62f304a310ef702ff4ee3b613a13b619db47721b
Ghevond <ghevond.98@gmail.com>
2019-03-10 15:17:25 +0400

    Scharr filter

--- ScharrEdgeDetection/Main_Scharr.cpp
+++ ScharrEdgeDetection/Main_Scharr.cpp
@@ -6,8 +6,8 @@
 
 namespace 
 {
-	const std::string input_path = "Files/SobelFilter/input.png";
-	const std::string output_path = "Files/SobelFilter/Result.png";
+	const std::string input_path = "Files/ScharrEdgeDetection/input.png";
+	const std::string output_path = "Files/ScharrEdgeDetection/Result.png";
 
 	void make_gray(cv::Mat& img) {
 	
@@ -40,42 +40,40 @@ namespace
 			result_image[i] = new int[size_y];
 		}
 
+		
+
 		for (size_t i = 1; i < img.rows - 1; i++)
 		{
 			for (size_t j = 1; j < img.cols - 1; j++)
 			{
-				auto left_top = img.at<cv::Vec3b>(i - 1,j - 1);
-				auto left = img.at<cv::Vec3b>(i,j - 1);
-				auto left_bottom = img.at<cv::Vec3b>(i  + 1,j - 1);
-				auto top = img.at<cv::Vec3b>(i-1,j);
+				auto left_top = img.at<cv::Vec3b>(i - 1,j - 1)[0];
+				auto left = img.at<cv::Vec3b>(i,j - 1)[0];
+				auto left_bottom = img.at<cv::Vec3b>(i  + 1,j - 1)[0];
+				auto top = img.at<cv::Vec3b>(i-1,j)[0];
 
-				auto right_top = img.at<cv::Vec3b>(i - 1,j + 1);
-				auto right = img.at<cv::Vec3b>(i,j + 1);
-				auto right_bottom = img.at<cv::Vec3b>(i + 1,j + 1);
-				auto bottom = img.at<cv::Vec3b>(i + 1, j);
+				auto right_top = img.at<cv::Vec3b>(i - 1,j + 1)[0];
+				auto right = img.at<cv::Vec3b>(i,j + 1)[0];
+				auto right_bottom = img.at<cv::Vec3b>(i + 1,j + 1)[0];
+				auto bottom = img.at<cv::Vec3b>(i + 1, j)[0];
 
-				int gx = (-1)*left_top[0] + (-2)*left[0] + (-1)*left_bottom[0] +
-					right_top[0] + (2) * right[0] + right_bottom[0];
+				double gx = (left_top*3 + 10*left + 3*left_bottom -
+					3*right_top - 10*right - 3*right_bottom)/16;
 
-				int gy = left_top[0] + (2)*top[0] + right_top[0] +
-					(-1)*left_bottom[0] + (-2) * bottom[0] + (-1)*right_bottom[0];
+				double gy = (3 * left_top + 10 * top + 3 * right_top -
+					3*left_bottom - 10*bottom  - 3*right_bottom)/16;
 
-				int g = std::sqrt(gx*gx + gy*gy);
+				double g = std::sqrt(gx*gx + gy * gy);
+
+
 				
-				/*if (g < 150) {
-					g = 255;
-				}
-				else {
-					g = 0;
-				}*/
-
-				if (g > 200) {
-					g = 255;
-				}
-
 				result_image[i][j] = g;
+
 			}
 		}
+
+		
+
+		
 
 		for (size_t i = 1; i < img.rows - 1; i++)
 		{
